63f565e7294fde374ae6d3fef7e9e263f637f7fc
LucasMarquesDePaula <marques.analista@gmail.com>
2017-11-16 22:39:20 -0200

    Hello ws

--- src/main/java/br/ufpr/tads/dac/ds/ws/WebService.java
+++ src/main/java/br/ufpr/tads/dac/ds/ws/WebService.java
@@ -1,8 +1,15 @@
 package br.ufpr.tads.dac.ds.ws;
 
+import br.ufpr.tads.dac.ds.facede.CrudFacede;
+import br.ufpr.tads.dac.ds.facede.EntregaFacede;
+import br.ufpr.tads.dac.ds.facede.ValidationException;
+import br.ufpr.tads.dac.ds.model.Entrega;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -30,5 +37,36 @@ public class WebService {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void put(Message message) {
+
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message post(Message message) throws ValidationException {
+        final Map<String, String> parameters = message.getParameters();
+        final Entrega model = new Entrega();
+
+        // Valores default
+        model.setCancelado((byte) 0x0);
+        model.setDataHoraCadastro(new Date());
+        model.setEntregaFrustrada((byte) 0x0);
+        model.setEntregue((byte) 0x0);
+
+        // Valores passados como parâmetro
+        model.setNomeCliente(parameters.get("nomeCliente"));
+        model.setEndereco(parameters.get("endereco"));
+        model.setObservacao(parameters.get("observacao"));
+        model.setPedidoId(Integer.parseInt(parameters.get("pedidoId")));
+
+        final CrudFacede<Entrega> facede = new EntregaFacede();
+
+        facede.save(model);
+
+        return new Message("ok", new HashMap<String, String>() {
+            {
+                put("entregaId", model.getId().toString());
+            }
+        });
     }
 }
